560aab9e7a75948b7b37990e03ad3c97538d20e6
xjakubeco <102169575+xjakubeco@users.noreply.github.com>
2022-03-31 23:15:09 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#toggleThis").click(function(){
+      $(".hide").css("display", "contents");
+      $("#toggleThis").css("display", "none");
+      $("#clickHeree").css("display", "none");
+      $("#clickHere").css("display", "none");
+    
+    });
+
+  });
